71ce37395efa56950918809c295330b4cacca9a6
Maria Aramayo <marialujanaramayo@gmail.com>
2022-08-13 18:31:37 -0300

    ultimos arreglos

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -4,10 +4,39 @@ const CartContext = createContext()
 
 export const CartContextProvider =({children}) => {
     const [cart,setCart] =useState([])
-  console.log(cart)
-  const addItem = (productToAdd) => {
-    setCart([...cart, productToAdd])
+    console.log(cart)
+    const addItem = (productToAdd) => {
+    if(!existeCart(productToAdd.id)){
+        setCart([...cart, productToAdd])
+    }else {
+        const cartUpdated = cart.map(prod => {
+            if(prod.id === productToAdd.id) {
+                const productUpdated = {
+                    ...prod,
+                    quantity: productToAdd.quantity
+                }
+                return productUpdated
+            } else {
+                return prod
+            }
+        })
+
+        setCart(cartUpdated)
+    }
   }
+
+  const limpiarCarrito = () => {
+    setCart([])
+  }
+
+  const eliminarItem = (id) => {
+    const nuevoCarritoSinProd = cart.filter(prod => prod.id !== id)
+    setCart(nuevoCarritoSinProd)
+    }
+
+  const existeCart = (id) => {
+    return cart.some(prod => prod.id === id)
+}
 
   const getQuantity = () =>{
     let acumulador = 0 
@@ -20,7 +49,7 @@ export const CartContextProvider =({children}) => {
   }
 
   return (
-    <CartContext.Provider value={{cart, addItem, getQuantity}}>
+    <CartContext.Provider value={{cart, addItem, getQuantity, existeCart,eliminarItem, limpiarCarrito }}>
         {children}
     </CartContext.Provider>
   )
