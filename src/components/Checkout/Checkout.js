2dcbb9faaffa8620c037133e8a6a145dc7bc8a1a
Maria Aramayo <marialujanaramayo@gmail.com>
2022-09-09 14:14:03 -0300

    falta estilos formulario

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -7,20 +7,30 @@ const Checkout = () => {
     const [isLoading, setIsLoading] = useState(false)
     const [orderCreated, setOrderCreated] = useState(false)
     const {cart, getQuantity, total, limpiarCarrito} = useContext(CartContext)
+    const [buyCode, setBuyCode] = useState('')
+    const [noData, setNoData] = useState('')
     const navigate = useNavigate()
 
     const totalCantidad = getQuantity();
     const compraTotal = total()
 
-    const crearOrden = async () => {
+    const crearOrden = async (e) => {
         setIsLoading(true)
+        e.preventDefault()
+        isNaN(e.target.campusPhone.value) && setNoData('numero de telefono incorrecto')
+        setTimeout(() => setNoData(''), 2500)
+        if( e.target.campusFirstName.value !== '' && e.target.campusPhone.value !== '' &&  e.target.campusLastName.value !== '' && e.target.campusAdress.value  !== '' && e.target.campusCity.value !== '' && e.target.campusState.value !== '' && e.target.campusEmail.value !== '' ){
         try {
             const objOrden = {
+                // Datos del comprador
                 buyer: {
-                    firstName:'Maria',
-                    lastName: 'Aramayo',
-                    phone:'12345678',
-                    address:'mi direccion es 123'
+                    firstName: e.target.campusFirstName.value,
+                    Lastname: e.target.campusLastName.value,
+                    adress: e.target.campusAdress.value,
+                    phone: e.target.campusPhone.value,
+                    city: e.target.campusCity.value,
+                    state: e.target.campusState.value,
+                    email: e.target.campusEmail.value,
                 },
                 items: cart,
                 totalCantidad,
@@ -52,6 +62,7 @@ const Checkout = () => {
                 await batch.commit()
                 const refOrden = collection(db, 'ordenes')
                 const ordenAgregada = await addDoc(refOrden, objOrden)
+                setBuyCode(ordenAgregada.id)
                 console.log(`El id de su orden es: ${ordenAgregada.id}`)
                 limpiarCarrito()
                 setOrderCreated(true)
@@ -63,9 +74,14 @@ const Checkout = () => {
             }
         }catch (error){
             console.log(error)
-        } finally {
+
+        }finally {
             setIsLoading(false)
         }
+    }else{
+        setNoData('faltan rellenar campos')
+        setTimeout(() => setNoData(''), 3000)
+    }
         
     }
 
@@ -75,16 +91,39 @@ const Checkout = () => {
     if(orderCreated){
         return <div>
             <h1>La orden fue creada correctamente </h1>
+            <h2>el codigo de tu orden es: {buyCode}</h2>
             <h3>Será redirigido a la pagina de inicio</h3>
         </div>
     }
 
-    return (
-        <>
-        <h1>Checkout</h1>
-        <button className="linkFinalizar" onClick={crearOrden}>Generar orden</button>
-        </>
-    )
+   
+        /* <button className="linkFinalizar" onClick={crearOrden}>Generar orden</button> */
+        return (
+            <>
+                <h1>Finalizar compra</h1>
+                <div >
+                    <form onSubmit={crearOrden }>
+                        <label>Nombre: </label>
+                        <input type='text' name="campusFirstName" placeholder="Nombre" />
+                        <label>Apellido: </label>
+                        <input type='text' name="campusLastName" placeholder="Apellido" />
+                        <label>Direccion: </label>
+                        <input type='text' name="campusAdress" placeholder="Direccion" />
+                        <label>Ciudad: </label>
+                        <input type='text' name="campusCity" placeholder="Ciudad" />
+                        <label>Telefono: </label>
+                        <input type='text' name="campusPhone" placeholder="Telefono" />
+                        <label>Provincia: </label>
+                        <input type='text' name="campusState" placeholder="Provincia" />
+                        <label>E-Mail: </label>
+                        <input type='email' name="campusEmail" placeholder="E-Mail" />
+                        <button className="linkFinalizar">Generar Orden</button>
+
+                    </form>
+                </div>
+                    <p className='noData'>{noData}</p>
+            </>
+        )
 }
 
 export default Checkout;
